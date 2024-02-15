6b31ebeaadb063d857a7b66ae23bf16ec0940314
Honey Singh <51957556+jaskaur18@users.noreply.github.com>
2024-02-15 22:49:48 +0530

    Automatically Update update_at table field on any changes onto that row

--- migrations/1706034800_create_user_table.sql
+++ migrations/1706034800_create_user_table.sql
@@ -14,6 +14,18 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+-- +goose StatementEnd
+
+-- Create triggers for updating "updated_at" column in various tables
+CREATE TRIGGER update_user_updated_at BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
 -- +goose Down
 DROP TABLE IF EXISTS "User";
